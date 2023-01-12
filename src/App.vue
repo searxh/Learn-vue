15f3ca134240832636816b867b23871590417fc3
search <search35453@gmail.com>
2023-01-12 10:37:03 +0700

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="container">
-    <h1>Hello World</h1>
+    <h1>Hello</h1>
+    <HeaderC />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-
-@Options({
-  components: {},
+import { defineComponent } from "vue";
+import HeaderC from "./components/HeaderC.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    HeaderC
+  },
 })
-export default class App extends Vue {}
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: "Poppins", sans-serif;
 }
+
 .container {
   max-width: 500px;
   margin: 30px auto;
@@ -32,6 +38,7 @@ body {
   padding: 30px;
   border-radius: 5px;
 }
+
 .btn {
   display: inline-block;
   background: #000;
@@ -45,12 +52,15 @@ body {
   font-size: 15px;
   font-family: inherit;
 }
+
 .btn:focus {
   outline: none;
 }
+
 .btn:active {
   transform: scale(0.98);
 }
+
 .btn-block {
   display: block;
   width: 100%;
---
+++ src/components/HeaderC.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>Task tracker</h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "HeaderC"
+})
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
