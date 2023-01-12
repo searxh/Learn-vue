cf2b204ca2c198b9ec0fe0606a9b854017304076
search <search35453@gmail.com>
2023-01-12 21:56:52 +0700

    updated eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,13 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "prettier",
   ],
+  plugins: ["prettier", "@typescript-eslint"],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
