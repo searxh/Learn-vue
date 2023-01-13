c8161128c1daf950da5fe71c337cbed086a3a932
search <search35453@gmail.com>
2023-01-13 21:34:50 +0700

    add proxy in devserver

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:5050",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" },
+      },
+    },
+  },
 });
