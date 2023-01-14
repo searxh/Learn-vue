80d8f21e83c071d7bdcb3331e80caede75fed4b4
search <search35453@gmail.com>
2023-01-14 19:44:07 +0700

    update footerC and aboutV

--- src/components/FooterC.vue
+++ src/components/FooterC.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <p>Copyright &copy; 2023</p>
-    <a href="/about">About</a>
+    <router-link to="/about">About</router-link>
   </footer>
 </template>
 
--- src/views/AboutV.vue
+++ src/views/AboutV.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>About</h1>
+  <div>
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go back</router-link>
+  </div>
 </template>
+
+<style scoped>
+div {
+  text-align: center;
+  padding-top: 2rem;
+}
+</style>
